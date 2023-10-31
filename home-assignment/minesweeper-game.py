5d973ee9971ef200c521786a5115b2c63da5256e
R-Zaib <rehanzaib95@protonmail.com>
2023-10-31 02:13:02 +0100

    updated the iteration of user input successfully  with while loop

--- home-assignment/minesweeper-game.py
+++ home-assignment/minesweeper-game.py
@@ -3,17 +3,16 @@
 import random
 
 # asking the user to enter grid size and number of mines to be placed in the grid
-try:
-    while True:
+while True:
+    try:    
         grid_size = int(input("Enter the board size in square to play: "))
         if grid_size > 0:
             break
         else:
             print("Error: Invalid input. Please enter a positive number (greater than zero).")  
-except ValueError:
-    print("Error: Invalid input. Please enter a positive number instead of a string.")
-# unable to reiterate the code when a string is entered, tried different ways to make it work
-# but it runs only once for now. It should also be in while loop i think.
+    except ValueError:
+        print("Error: Invalid input. Please enter a positive number instead of a string.")
+    
 
 # except ValueError:
 #     print("Error: Invalid input. Please enter a positive number instead of a string.")
@@ -24,16 +23,19 @@ except ValueError:
 
 
 while True:
-    num_mines = int(input("Enter the number of mine to be placed in the board: "))
-    if 0 < num_mines and num_mines > (((grid_size ** 2) * 0.8) // 1):
-        break
-    else:
-        print("Error: Invalid input. Please enter a number in range of ", grid_size, "and", grid_size**2)
+    try:
+        num_mines = int(input("Enter the number of mine to be placed in the board: "))
+        if 0 < num_mines and num_mines < (((grid_size ** 2) * 0.8) // 1):
+            break
+        else:
+            print("Error: Invalid input. Please enter a number in range of ", grid_size, "and", grid_size**2)
+    except ValueError:
+        print("Error: Invalid input. Please enter a positive number instead of a string.")
 
-# what if the number of mines the user entered exceeds the total number of placeholders in the board?
+print("Let's begin the minesweeper game!")
 
 # using the input to create a nested list, which serves as a 2D board.
-# board_grid = [["#"for i in range(grid_size)] for j in range(grid_size)]
+board_grid = [["#"for i in range(grid_size)] for j in range(grid_size)]
 
 # placing the mines using random
 # mine_location = random.sample(range(grid_size ** 2), num_mines)
