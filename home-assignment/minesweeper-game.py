146da122cbed9d78d5236847f8eb584185699cd1
Don <don@Dons-MacBook-Air.local>
2023-11-08 06:14:59 +0100

    added and displayed the miens on game board

--- home-assignment/minesweeper-game.py
+++ home-assignment/minesweeper-game.py
@@ -2,6 +2,7 @@
 
 import random
 import string
+import copy
 
 
 # def game_loop():
@@ -11,7 +12,7 @@ import string
 # if it says its B18, it should check if it is within the game paramenters
 
 # asking the user to enter the game parameters to play
-def game_input_parameters():
+def get_game_input_parameters():
     while True:         # asking user for grid size to play
         try:    
             grid_size = int(input("Enter the board to play: "))
@@ -26,7 +27,7 @@ def game_input_parameters():
         try:
             num_mines = int(input("Enter the number of mine to be placed in the board: "))
             max_mines = int(grid_size ** 2 * 0.8 // 1)
-            if 0 < num_mines and num_mines < max_mines:
+            if grid_size <= num_mines and num_mines <= max_mines:
                 break
             else:
                 print("Error: Invalid input. Please enter a number in range of ", grid_size, "and", max_mines)
@@ -36,15 +37,20 @@ def game_input_parameters():
     print("Let's begin the minesweeper game!")
     return grid_size, num_mines
 
-# using the input to create a nested list, which serves as a 2D board with its headings
-board_grid = [["#" for i in range(grid_size + 1)] for j in range(grid_size + 1)]
-board_grid[0] = [" "] + list(range(1, grid_size + 1))   # to add headings as 1, 2, 3, ...
-# should I convert the board_grid[0] to "str" so they don't count 
-# when mines nearby are being calculated
+def game_array(board_grid):
+    grid_size = len(board_grid)
+    board_grid_number_header = [list(range(1, grid_size + 1))]
+    board_grid_representation = board_grid_number_header + copy.deepcopy(board_grid)
 
-for row_chr in range(1, grid_size + 1):
-    board_grid[row_chr][0] = string.ascii_uppercase[row_chr - 1]  # using ASCII values to get A, B, C, ...
+    aphabets_indices = " " + string.ascii_uppercase[:grid_size] # string/list/tuple slicing applicable to **ordered** data structure
+    for row_index, row in enumerate(board_grid_representation):
+        row.insert(0, aphabets_indices[row_index])
+    return board_grid_representation
 
+# printing the board size in 2D shape with rows and columns as grid_size
+def displayBoard(grid):
+    for row in grid:
+        print(' '.join([str(cell) for cell in row]))
 
 # determining the mines locations using random
 def determine_mines_location(num_mines, grid_size):
@@ -58,15 +64,11 @@ def determine_mines_location(num_mines, grid_size):
     return mine_location
         
 def place_mines(board, mine_locations):
+    grid_size = len(board)
     for location in mine_locations:
-        x = location[0]
-        y = location[1]
-        board[x][y] = 'X'
-
-# printing the board size in 2D shape with rows and columns as grid_size
-def displayBoard():
-    for row in board_grid:
-        print(' '.join([str(cell) for cell in row]))
+        x, y = location
+        if 0 <= x < grid_size and 0 <= y < grid_size:
+            board[x][y] = "X"
 
 
 # validating the input to reveal a field from the board
@@ -83,28 +85,38 @@ def valid_reveal_input(alpha_str, valid_alphabets, grid_size):
 
 # reference https://stackoverflow.com/questions/40209158/checking-if-an-input-is-formatted-correctly-in-python-3
 
-displayBoard()    
+# data structure
+
+grid_size, num_mines = get_game_input_parameters()                          # call the function to get grid_size and num_mines
+board_grid = [["#" for i in range(grid_size)] for j in range(grid_size)]    # to create 2D array
+mines_location = determine_mines_location(grid_size, num_mines)             # extracting mine locations
+place_mines(board_grid, mines_location)                                     # call game_array function to extract grid representation
+board_grid_representation = game_array(board_grid)
+displayBoard(board_grid_representation)
 
 
 # creating a game loop
 
-# asking the user to reveal a field
+
+row_alphabets = string.ascii_uppercase[:grid_size]
 while True:
-    reveal = input("Which field to reveal?")
-    if valid_reveal_input(reveal):
+    reveal = input("Which field to reveal?")            # asking the user to reveal a field
+    if valid_reveal_input(reveal, row_alphabets,grid_size):
         print("Let's see if you hit a mine!")
     else:
-        print("Error! Invalid input: Please enter a valid field (A1, B2, C3...).")
+        print("Error! Invalid input: Please enter a valid field (A1, B2, C3...).")    
+
 
 # determining the neighbouring mines in board_grid
-def neighbouring_mines(row, col, grid_size, baord_grid):
-    mine_count = 0      # total number of mines nearby
-    for r in range(row - 1, row + 2):
-        for c in range(col - 1, col + 2):
-            if 0 <= r < grid_size and 0 <= c < grid_size:
-                if board_grid[r][c] == "X":         # representing "X" here for a mine
-                    mine_count += 1
-    return mine_count
+
+# def neighbouring_mines(row, col, grid_size, baord_grid):
+#     mine_count = 0      # total number of mines nearby
+#     for r in range(row - 1, row + 2):
+#         for c in range(col - 1, col + 2):
+#             if 0 <= r < grid_size and 0 <= c < grid_size:
+#                 if board_grid[r][c] == "X":         # representing "X" here for a mine
+#                     mine_count += 1
+#     return mine_count
 
 # previously tried this with both while and for loop, but apparently it has errors.
 # then fine-tuned the code and asked ChatGPT to evaluate the final version
@@ -134,10 +146,10 @@ def neighbouring_mines(row, col, grid_size, baord_grid):
 #check within my function, whether it is over or not
 #situations in which the game is over.
 
-    """ 
-    hide it or have a completely separate data structure (recommended) - for the showing part
-    use slices to create new list, it produces slice from start to end-1   my_list[start:end]
-    """
+    # """ 
+    # hide it or have a completely separate data structure (recommended) - for the showing part
+    # use slices to create new list, it produces slice from start to end-1   my_list[start:end]
+    # """
 
 # print('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
 #       for row in board_grid]))
