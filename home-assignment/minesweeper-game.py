bea9a1272742d3a1805b08ddb2d6b56f4f9dbfdb
Don <don@Dons-MacBook-Air.local>
2023-11-08 03:02:05 +0100

    converted code into diff functions and added mines return function

--- home-assignment/minesweeper-game.py
+++ home-assignment/minesweeper-game.py
@@ -10,30 +10,31 @@ import string
 #this will be the main game loop , have to handle the guesses of the user here, 
 # if it says its B18, it should check if it is within the game paramenters
 
-# asking the user to enter grid size
-while True:
-    try:    
-        grid_size = int(input("Enter the board size in square to play: "))
-        if grid_size > 0:
-            break
-        else:
-            print("Error: Invalid input. Please enter a positive number (greater than zero).")  
-    except ValueError:
-        print("Error: Invalid input. Please enter a positive number instead of a string.")
-    
+# asking the user to enter the game parameters to play
+def game_input_parameters():
+    while True:         # asking user for grid size to play
+        try:    
+            grid_size = int(input("Enter the board to play: "))
+            if grid_size > 0:
+                break
+            else:
+                print("Error: Invalid input. Please enter a positive number. ")  
+        except ValueError:
+            print("Error: Invalid input. Please enter a positive number.")
+        
+    while True:         # asking for number of mines to be placed
+        try:
+            num_mines = int(input("Enter the number of mine to be placed in the board: "))
+            max_mines = int(grid_size ** 2 * 0.8 // 1)
+            if 0 < num_mines and num_mines < max_mines:
+                break
+            else:
+                print("Error: Invalid input. Please enter a number in range of ", grid_size, "and", max_mines)
+        except ValueError:
+            print("Error: Invalid input. Please enter a positive number.")    
 
-# asking the user to input the number of mines to be placed in the board
-while True:
-    try:
-        num_mines = int(input("Enter the number of mine to be placed in the board: "))
-        if 0 < num_mines and num_mines < (((grid_size ** 2) * 0.8) // 1):
-            break
-        else:
-            print("Error: Invalid input. Please enter a number in range of ", grid_size, "and", grid_size**2)
-    except ValueError:
-        print("Error: Invalid input. Please enter a positive number instead of a string.")
-
-print("Let's begin the minesweeper game!")
+    print("Let's begin the minesweeper game!")
+    return grid_size, num_mines
 
 # using the input to create a nested list, which serves as a 2D board with its headings
 board_grid = [["#" for i in range(grid_size + 1)] for j in range(grid_size + 1)]
@@ -45,15 +46,22 @@ for row_chr in range(1, grid_size + 1):
     board_grid[row_chr][0] = string.ascii_uppercase[row_chr - 1]  # using ASCII values to get A, B, C, ...
 
 
-# placing the mines using random
-mine_location = []
-while num_mines > len(mine_location):
-    r_rand = random.randint(0, grid_size - 1)
-    c_rand = random.randint(0, grid_size - 1)
-    
-    if (r_rand, c_rand) not in mine_location:
-        mine_location.append((r_rand, c_rand))
-       
+# determining the mines locations using random
+def determine_mines_location(num_mines, grid_size):
+    mine_location = []
+    while num_mines > len(mine_location):
+        row = random.randint(0, grid_size - 1)
+        col = random.randint(0, grid_size - 1)
+        
+        if (row, col) not in mine_location:
+            mine_location.append((row, col))
+    return mine_location
+        
+def place_mines(board, mine_locations):
+    for location in mine_locations:
+        x = location[0]
+        y = location[1]
+        board[x][y] = 'X'
 
 # printing the board size in 2D shape with rows and columns as grid_size
 def displayBoard():
@@ -62,14 +70,14 @@ def displayBoard():
 
 
 # validating the input to reveal a field from the board
-def valid_reveal_input(alpha_str):
+def valid_reveal_input(alpha_str, valid_alphabets, grid_size):
     if len(alpha_str) != 2:
         return False
     letter = alpha_str[0]
     number = alpha_str[1]
-    if not letter.isalpha() or not letter.isupper():
+    if not letter.isalpha() or not letter.isupper() or not letter in valid_alphabets:
         return False
-    if not number.isdigit() or int(number) < 1:
+    if not number.isdigit() or int(number) < 1 or int(number) > grid_size:
         return False
     return True
 
